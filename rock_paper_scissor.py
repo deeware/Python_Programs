91d9641fb95781258735c7cd7d1916bcc66cf5d4
deeware <55303607+deeware@users.noreply.github.com>
2020-03-04 16:34:13 +0530

    Add files via upload

---
+++ rock_paper_scissor.py
@@ -0,0 +1,30 @@
+print('Stone : s','Paper : p','Scissor : c')
+
+a=b=0
+chance=0
+while a==b or chance<3:
+             p1=input('player 1 :')
+             p2=input('player 2 :')
+             chance+=1
+             if p1==p2:
+                          w='draw'
+             elif p1=='s' and p2=='p':
+                          b+=1
+                          w='player2'
+             elif p1=='p' and p2=='s':
+                          a+=1
+                          w='player1'
+             elif p1=='p' and p2=='c':
+                          b+=1
+                          w='player2'
+             elif p1=='c' and p2=='p':
+                          a+=1
+                          w='player1'
+             elif p1=='c' and p2=='s':
+                          b+=1
+                          w='player2'
+             elif p1=='s' and p2=='c':
+                          a+=1
+                          w='player2'
+
+print('Winner : ','plyer2' if b>a else 'player1')
